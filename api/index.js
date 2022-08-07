eac5de34e92ea8a988dc99d3f79994987b4cea8a
akmhmgc <akimuhamaguchi@icloud.com>
2022-08-07 18:42:27 +0900

    Retrun svg random number

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+}
+
+const svg = () => {
+  return `
+          <svg xmlns="http://www.w3.org/2000/svg" width="180" height="120">
+            <text x="0" y="20" transform="rotate(30 20,40)">
+              ランダムナンバー: ${getRandomInt(100)}
+            </text>
+          </svg>
+          `;
+}
+
 export default async (req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-  return res.send('hello world')
+  res.setHeader("Content-Type", "image/svg+xml");
+  return res.send(svg())
 }
