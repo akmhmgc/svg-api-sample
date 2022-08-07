3bd4eb36207b0f5dc24848b94664457903dbdefa
akmhmgc <akimuhamaguchi@icloud.com>
2022-08-07 20:48:48 +0900

    Add animation

--- api/index.js
+++ api/index.js
@@ -4,9 +4,29 @@ const getRandomInt = (max) => {
 
 const svg = () => {
   return `
-          <svg xmlns="http://www.w3.org/2000/svg" width="180" height="120">
-            <text x="0" y="20" transform="rotate(30 20,40)">
-              ランダムナンバー: ${getRandomInt(100)}
+          <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+          <style>
+            .header {
+              fill: red;
+              animation:center 1s 1;
+            }
+            #fuuno {
+              transform-origin: center;
+              animation:omikuji 1s ease-in-out forwards;
+              }
+              @keyframes omikuji {
+                0% {
+                  transform:scale(1) rotate(0deg);
+                  opacity:0;
+                }
+                100% {
+                  transform:scale(5) rotate(360deg);
+                  opacity:1;
+                }
+              }
+              </style>
+            <text class="header" id="fuuno" x="100" y="100">
+              ${getRandomInt(100)}
             </text>
           </svg>
           `;
